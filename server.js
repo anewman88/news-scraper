ff9a8c763e7743dfac61b7d97fd32bf319624aaa
Ann Newman <anewman1822@gmail.com>
2019-07-13 08:19:12 -0500

    added code to allow deployment on heroku

--- server.js
+++ server.js
@@ -32,7 +32,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/NasaNews", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/NasaNews";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 // =============================================================
